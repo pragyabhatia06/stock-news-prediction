60170c27fd86bf471fb08c96709f38332e8862e8
pragyabhatia06 <119414834+pragyabhatia06@users.noreply.github.com>
2024-09-08 23:25:34 +0000

    reload

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.append(nltk_data_dir)
 
 # Function to download NLTK resources
-@st.cache_resource
+# @st.cache_resource
 def download_nltk_data():
     nltk.download('punkt', download_dir=nltk_data_dir)
     nltk.download('stopwords', download_dir=nltk_data_dir)
